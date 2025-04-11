4d28c1c3dbc70ddd2b469811184f76a4c3e2bd5d
Shivam Maurya <sm5708251@gmail.com>
2025-04-11 20:45:33 +0530

    Change routes

--- fronted/src/components/Header.jsx
+++ fronted/src/components/Header.jsx
@@ -181,7 +181,7 @@ const Navbar = () => {
                       User List
                     </Link>
                     <Link
-                      to="/mentor"
+                      to="/our-mentor"
                       className="block px-4 py-2 hover:bg-gray-100"
                     >
                       Mentor
