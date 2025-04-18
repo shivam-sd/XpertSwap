db026e06d915b83781e71f7c5c42e15981bfbfd4
Shivam Maurya <sm5708251@gmail.com>
2025-04-18 19:32:12 +0530

    Add DataBase Cluster

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const ConnectDataBase = () => {
-    mongoose.connect(`${process.env.DB_URL}`).then((res) => {
+    mongoose.connect(`${process.env.MONGO_URI}`).then((res) => {
         console.log("Database Connected To The Atlas");
     }).catch((err) => {
         console.log("Error in Database Connection" , err);
