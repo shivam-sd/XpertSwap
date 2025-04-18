1989ba60d7b55b8190b6e2d973dbd2d6feaa2a53
Shivam Maurya <sm5708251@gmail.com>
2025-04-18 19:40:00 +0530

    DataBase Connect From Atlas

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
 
 const ConnectDataBase = () => {
-    mongoose.connect(`${process.env.MONGO_URI}`).then((res) => {
-        console.log("Database Connected To The Atlas");
-    }).catch((err) => {
-        console.log("Error in Database Connection" , err);
-    });
+    console.log("Connecting to DB:", process.env.MONGO_URI); // For debug
+    mongoose.connect(process.env.MONGO_URI)
+        .then(() => {
+            console.log("Database Connected To The Atlas");
+        })
+        .catch((err) => {
+            console.log("Error in Database Connection", err);
+        });
 };
 
 module.exports = ConnectDataBase;
