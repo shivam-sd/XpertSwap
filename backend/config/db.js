e949fc99a9750246314a803fe3767649b5633f65
Shivam Maurya <sm5708251@gmail.com>
2025-04-18 19:26:27 +0530

    Add IP

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const ConnectDataBase = () => {
-    mongoose.connect(`mongodb+srv://py0504955:${process.env.DB_PASSWORD}@skilllinkr.mrkhdx4.mongodb.net/?retryWrites=true&w=majority&appName=SkillLinkr`).then((res) => {
+    mongoose.connect(`${process.env.DB_URL}`).then((res) => {
         console.log("Database Connected To The Atlas");
     }).catch((err) => {
         console.log("Error in Database Connection" , err);
