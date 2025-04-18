7eac658cca965952cefa276136bf2daf25d7db97
Shivam Maurya <sm5708251@gmail.com>
2025-04-18 20:05:51 +0530

    changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,26 +1,26 @@
 const mongoose = require("mongoose");
 
-// const ConnectDataBase = () => {
-//     console.log("Mongo URI:", process.env.MONGO_URI); // Debug line
+const ConnectDataBase = () => {
+    console.log("Mongo URI:", process.env.MONGO_URI); // Debug line
 
-//     mongoose.connect(process.env.MONGO_URI)
-//         .then(() => console.log("Database Connected To The Atlas"))
-//         .catch((err) => console.log("Error in Database Connection", err));
-// };
+    mongoose.connect(process.env.MONGO_URI)
+        .then(() => console.log("Database Connected To The Atlas"))
+        .catch((err) => console.log("Error in Database Connection", err));
+};
 
-// module.exports = ConnectDataBase;
+module.exports = ConnectDataBase;
 
 
 
 // for localhost
 
 
-const ConnectDataBase = () => {
-    mongoose.connect(`${process.env.MONGO_URI}`).then((res) => {
-        console.log("Database Connected To The Atlas");
-    }).catch((err) => {
-        console.log("Error in Database Connection" , err);
-    });
-};
+// const ConnectDataBase = () => {
+//     mongoose.connect(`${process.env.MONGO_URI}`).then((res) => {
+//         console.log("Database Connected To The Atlas");
+//     }).catch((err) => {
+//         console.log("Error in Database Connection" , err);
+//     });
+// };
 
-module.exports = ConnectDataBase;
+// module.exports = ConnectDataBase;
